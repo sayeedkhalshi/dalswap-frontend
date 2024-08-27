b2a7cf33be452b0a1553848df4c5601b2320539f
Md. Abu Sayeed Mondal <sayeedmondal1412@gmail.com>
2024-08-27 15:30:32 +0600

    rainbow kit and redux setup

---
+++ src/components/navbar/Menu.tsx
@@ -0,0 +1,60 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+// import LogoImage from "./LogoImage";
+import LogoText from "./LogoText";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+const Menu = () => {
+    return (
+        <nav className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div className="relative flex items-center justify-between">
+                <div className="flex items-center px-2 lg:px-0 mx-auto">
+                    {/* <LogoImage /> */}
+                    <LogoText />
+                    <div className=" lg:block lg:ml-6">
+                        <div className="flex space-x-4">
+                            <Link href="/app" className="nav-link">
+                                App
+                            </Link>
+                            <Link
+                                href="https://github.com/sayeedkhalshi/learnea-frontend"
+                                className="nav-link"
+                                target="_blank"
+                            >
+                                Code
+                            </Link>
+                            <Link
+                                href="https://github.com/sayeedkhalshi/learnea-dev-docs/blob/main/initial-philosophy-of-app.md"
+                                className="nav-link"
+                                target="_blank"
+                            >
+                                Whitepaper
+                            </Link>
+                            <Link
+                                href="https://docs.google.com/document/d/1CZJZGOgpnhuC0wVHnj1gvs3N3kLhJ7qmOSfRMLAWNlc/edit?usp=sharing"
+                                className="nav-link"
+                                target="_blank"
+                            >
+                                DevDoc
+                            </Link>
+                            <Link
+                                href="https://twitter.com/sayeedkhalshi"
+                                className="nav-link"
+                                target="_blank"
+                            >
+                                Contact
+                            </Link>
+
+                            <div className="ff-monospace">
+                                <ConnectButton label="Login" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Menu;
---
+++ src/lib/constants.ts
@@ -0,0 +1,7 @@
+export const API = "http://localhost:3000";
+
+const learnea_v1_contract = "0x24B9a48EBf650c7ddB042c0b84859b8595b67d2C";
+const learneaContractAddresses: `0x${string}`[] = [learnea_v1_contract];
+
+export const learnea_contract_address =
+    learneaContractAddresses[learneaContractAddresses.length - 1];
